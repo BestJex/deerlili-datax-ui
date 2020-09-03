ff06e38e3c8966b6289ff7b41dc10bb7089f445b
deerlili <deerlili@163.com>
2020-09-03 23:05:10 +0800

    2020-09-03 修改首页监控

--- src/views/dashboard/admin/components/BarChart.vue
+++ src/views/dashboard/admin/components/BarChart.vue
@@ -1,47 +1,68 @@
 <template>
-  <div :class="className" :style="{height:height,width:width}" />
+  <div class="app-container">
+    <!-- <div class="filter-container">
+      <el-input v-model="listQuery.registryKey" placeholder="执行器" style="width: 200px;" class="filter-item" />
+      <el-button v-waves class="filter-item" type="primary" icon="el-icon-search" @click="fetchData">
+        Search
+      </el-button>
+    </div> -->
+    <div v-for="item in list" :key="item.id" class="container">
+      <div style="text-align:center;"><el-tag style="color:#da5416;font-size: 18px;">执行器：{{ item.registryKey }}</el-tag></div>
+      <el-row>
+        <!-- <el-col :span="12"><el-tag>注册地址：{{ item.registryValue }}</el-tag></el-col>
+        <el-col :span="12"><el-tag>更新时间：{{ item.updateTime | parseTime('{y}-{m}-{d} {h}:{i}') }}</el-tag>
+        </el-col> -->
+      </el-row>
+      <el-tag>注册地址：{{ item.registryValue }}</el-tag>
+      <el-tag>更新时间：{{ item.updateTime }}</el-tag>
+      <br>
+      <div :class="item.id + ' fl'" style="width: 50%;height: 200px;color:#da5416;" />
+      <div :class="item.id + ' fl'" style="width: 50%;height: 200px;color:#da5416;" />
+      <!-- <div :class="item.id + ' fl' + ' loadAverage'" style="width: 30%;height: 300px ">
+        <p class="title">平均负载</p>
+        <p class="number">{{ item.loadAverage >= 0 ? item.loadAverage : 0 }}</p>
+      </div> -->
+
+    </div>
+  </div>
 </template>
 
 <script>
-import echarts from 'echarts'
-require('echarts/theme/macarons') // echarts theme
-import resize from './mixins/resize'
 import { getList } from '@/api/datax-registry'
-
-const animationDuration = 6000
+import waves from '@/directive/waves'
+import parseTime from '@/utils'
 
 export default {
-  mixins: [resize],
-  props: {
-    className: {
-      type: String,
-      default: 'chart'
-    },
-    width: {
-      type: String,
-      default: '100%'
-    },
-    height: {
-      type: String,
-      default: '300px'
+  name: 'Registry',
+  directives: { waves, parseTime },
+  filters: {
+    statusFilter(status) {
+      const statusMap = {
+        published: 'success',
+        draft: 'gray',
+        deleted: 'danger'
+      }
+      return statusMap[status]
     }
   },
   data() {
     return {
-      chart: null
+      list: null,
+      listLoading: true,
+      total: 0,
+      listQuery: {
+        current: 1,
+        size: 10,
+        registryKey: undefined
+      },
+      dialogPluginVisible: false
     }
+  },
+  created() {
+    this.fetchData()
   },
   mounted() {
-    this.$nextTick(() => {
-      this.initChart()
-    })
-  },
-  beforeDestroy() {
-    if (!this.chart) {
-      return
-    }
-    this.chart.dispose()
-    this.chart = null
+
   },
   methods: {
     fetchData() {
@@ -55,65 +76,109 @@ export default {
         this.listLoading = false
         this.$nextTick(function() {
           for (let i = 0; i < this.list.length; i++) {
-            this.initChart(this.list[i])
+            this.initEcharts(this.list[i])
           }
         })
       })
     },
-    initChart() {
-      this.chart = echarts.init(this.$el, 'macarons')
-
-      this.chart.setOption({
+    initEcharts(data) {
+      const myChart1 = this.$echarts.init(document.getElementsByClassName(data.id)[0])
+      // 绘制图表
+      var option1 = {
+        title: {
+          text: 'cpu使用率',
+          subtext: '',
+          x: 'center'
+        },
         tooltip: {
-          trigger: 'axis',
-          axisPointer: { // 坐标轴指示器，坐标轴触发有效
-            type: 'line' // 默认为直线，可选为：'line' | 'shadow'
-          }
+          formatter: '{a}: {c}%'
         },
-        grid: {
-          top: 10,
-          left: '2%',
-          right: '2%',
-          bottom: '3%',
-          containLabel: true
+        toolbox: {
+          feature: {
+            restore: {},
+            saveAsImage: {}
+          },
+          show: false
         },
-        xAxis: [{
-          type: 'category',
-          data: ['192.168.8.118:9999', '192.168.8.119:9999'],
-          axisTick: {
-            alignWithLabel: true
-          }
-        }],
-        yAxis: [{
-          type: 'value',
-          axisTick: {
-            show: false
-          }
-        }],
         series: [{
-          name: 'CPU使用率',
-          type: 'bar',
-          stack: 'vistors',
-          barWidth: '60%',
-          data: [40, 60, 200],
-          animationDuration
-        }, {
-          name: '内存使用率',
-          type: 'bar',
-          stack: 'vistors',
-          barWidth: '60%',
-          data: [40, 50, 200],
-          animationDuration
-        }, {
-          name: '负载',
-          type: 'bar',
-          stack: 'vistors',
-          barWidth: '60%',
-          data: [2, 1, 0],
-          animationDuration
+          name: 'cpu使用率',
+          type: 'gauge',
+          max: 100,
+          radius: '100%', // 半径
+          startAngle: 215, // 起始位置
+          endAngle: -35, // 终点位置
+          detail: {
+            formatter: '{value}%'
+          },
+          data: [{
+            value: data.cpuUsage,
+            name: ''
+          }]
         }]
-      })
+      }
+      myChart1.setOption(option1)
+
+      const myChart2 = this.$echarts.init(document.getElementsByClassName(data.id)[1])
+      // 绘制图表
+      var option2 = {
+        title: {
+          text: '内存使用率',
+          subtext: '',
+          x: 'center'
+        },
+        tooltip: {
+          formatter: '{a}: {c}%'
+        },
+        toolbox: {
+          feature: {
+            restore: {},
+            saveAsImage: {}
+          },
+          show: false
+        },
+        series: [{
+          name: '内存使用率',
+          type: 'gauge',
+          max: 100,
+          radius: '100%', // 半径
+          startAngle: 215, // 起始位置
+          endAngle: -35, // 终点位置
+          detail: {
+            formatter: '{value}%'
+          },
+          data: [{
+            value: data.memoryUsage,
+            name: ''
+          }]
+        }]
+      }
+      myChart2.setOption(option2)
     }
   }
 }
 </script>
+<style lang="scss" scope>
+  .container{
+    overflow: hidden;
+    p{
+      font-size: 14px;color: rgb(102, 102, 102);padding: 10px 0;
+      .fl{
+        float: left;
+      }
+      .fr{
+        float: right;
+      }
+    }
+    .loadAverage{
+      p{
+        text-align: center;
+      }
+      .title{
+        font-size: 18px;font-weight: bold;color: #333;padding: 5px 0;margin: 0;
+      }
+      .number{
+        font-size: 50px;color: #da5416
+      }
+    }
+  }
+</style>
